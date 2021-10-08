511c2739e4efdb0dd4acae5618339f92fda79519
rednand <nan-aguiar@hotmail.com>
2021-10-08 14:30:17 -0300

    alteração cadastraCliente.js

--- projeto_inicial/admin/controller/cadastraClientes-controller.js
+++ projeto_inicial/admin/controller/cadastraClientes-controller.js
@@ -1,18 +1,17 @@
-import {
-    clienteService
-} from "../service/cliente-service.js"
+import { clienteService } from '../service/cliente-service.js'
 
-const formulario = document.querySelector("[data-form]")
+const formulario = document.querySelector('[data-form]')
 
 //executa algo assim que houver o submit
 //evento procura exatamento onde esta acontecendo
-formulario.addEventListener("submit", (evento) => {
-    evento.preventDefault()
-    const nome = evento.target.querySelector("[data-nome]").value
-    const email = evento.target.querySelector("[data-email]").value
 
-    clienteService.criaCliente(nome, email)
-        .then(() => {
-            window.location.href = '../telas/cadastro_produto.html'
-        })
+formulario.addEventListener('submit', (evento)=> { 
+  evento.preventDefault()
+  const nome = evento.target.querySelector('[data-nome]').value
+  const email = evento.target.querySelector('[data-email]').value
+
+  clienteService.criaCliente(nome, email)
+  .then(()=> {
+    window.location.href = '../telas/cadastro_concluido.html'
+  })
 })
